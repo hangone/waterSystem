90e926432bb24d8b92b9dd85895e472818aa64f4
abandon888 <1876746719@qq.com>
2023-04-14 21:50:18 +0800

    对数据更新页面微调

--- src/Services/waterQuality/realData.js
+++ src/Services/waterQuality/realData.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
-export const filterData = async () => {
+export const realData = async () => {
   try {
     const response = await axios.get(`https://water.miraitowa.tk/realtimeData`);
     const data = response.data
-    return data;
+    let arr = [];
+  for (let i = 0; i < data.length; i++) {
+    let obj = {
+      update_time: data[i].update_time,
+      temperature: data[i].temperature,
+    };
+    arr.push(obj);
+  }
+  return arr; 
   } catch (error) {
     throw new Error('获取水质数据失败，请稍后再试');
   }
 };
+
+
 // Call filterData every 10 seconds
-//setInterval(filterData, 10000);
+// setInterval(realData, 10000);
