a507c118d9c8c8f1578ea3aac66c45f17defbce8
abandon888 <1876746719@qq.com>
2023-04-16 21:57:21 +0800

    完成两个水电站的位置引入

--- src/Components/MapDam/index.jsx
+++ src/Components/MapDam/index.jsx
@@ -1,37 +1,41 @@
-import ReactDOM from 'react-dom';
-import React, { useState, useRef } from 'react';
-import { Map, Marker, APILoader, InfoWindow } from '@uiw/react-amap';
+import ReactDOM from 'react-dom'
+import React, { useState, useRef } from 'react'
+import { Map, Marker, APILoader, InfoWindow } from '@uiw/react-amap'
 
 const Example = () => {
-  const [show, setShow] = useState(false);
-  const [winPosition, setWinPosition] = useState();
-  const [content, setContent] = useState('<div>高德软件</div>');
+  const [show, setShow] = useState(false)
+  const [winPosition, setWinPosition] = useState()
+  const [content, setContent] = useState('<div>高德软件</div>')
   return (
     <>
-      <input type="text" value={content} onChange={(evn) => setContent(evn.target.value)}/>
+      <input
+        type="text"
+        value={content}
+        onChange={(evn) => setContent(evn.target.value)}
+      />
       <div style={{ width: '100%', height: '300px' }}>
-        <Map zoom={14} center={[116.397637, 39.900001]}>
+        <Map zoom={14} center={[115.466706, 25.273481]}>
           <Marker
             title="北京市"
-            position={new window.AMap.LngLat(116.405285,39.904989)}
+            position={new window.AMap.LngLat(115.466706, 25.273481)}
             onClick={(evn) => {
               if (!show) {
-                setWinPosition(new window.AMap.LngLat(116.405285,39.904989));
-                setShow(true);
+                setWinPosition(new window.AMap.LngLat(115.466706, 25.273481))
+                setShow(true)
               } else {
-                setWinPosition(new window.AMap.LngLat(116.405285,39.904989));
+                setWinPosition(new window.AMap.LngLat(115.466706, 25.273481))
               }
             }}
           />
           <Marker
             title="北京市"
-            position={new window.AMap.LngLat(116.415285,39.905589)}
+            position={new window.AMap.LngLat(115.507778, 25.291389)}
             onClick={(evn) => {
               if (!show) {
-                setWinPosition(new window.AMap.LngLat(116.415285,39.905589));
-                setShow(true);
+                setWinPosition(new window.AMap.LngLat(115.507778, 25.291389))
+                setShow(true)
               } else {
-                setWinPosition(new window.AMap.LngLat(116.415285,39.905589));
+                setWinPosition(new window.AMap.LngLat(115.507778, 25.291389))
               }
             }}
           />
@@ -39,23 +43,23 @@ const Example = () => {
             <InfoWindow
               visiable={show}
               position={winPosition}
-              offset={{ x: 0, y: -10}}
+              offset={{ x: 0, y: -10 }}
               content={content}
               onClose={(evn) => {
-                console.log('evn2', evn, show);
+                console.log('evn2', evn, show)
               }}
             />
           )}
         </Map>
       </div>
     </>
-  );
+  )
 }
 
 const MountMap = () => (
   <APILoader akey="a7a90e05a37d3f6bf76d4a9032fc9129">
     <Example />
   </APILoader>
-);
+)
 
-export default MountMap;
+export default MountMap
